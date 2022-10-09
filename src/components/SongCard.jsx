caf1e6e86706989170f27d3510af3e1600291f71
Vishakha Sahu <vishakhasahu023@gmail.com>
2022-10-09 20:48:49 +0530

    version - 3 right and left side bars added

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -17,7 +17,6 @@ const SongCard = ({ song, isPlaying, activeSong, i, data }) => {
     dispatch(playPause(true));
   }
 
-
   return (
     <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer " >
       <div className="relative w-full h-56 group" >
