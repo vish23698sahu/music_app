974bc8448f8e1de7420b6c0c117dfbe7b9d18df3
Vishakha Sahu <vishakhasahu023@gmail.com>
2022-10-12 23:02:38 +0530

    v - 6 Search functionality added

--- src/components/DetailsHeader.jsx
+++ src/components/DetailsHeader.jsx
@@ -13,7 +13,7 @@ const DetailsHeader = ({ artistId, artistData, songData }) => {
           alt="art"
           src={artistId ? artist.artwork?.url.replace('{w}', '500').replace('{h}', '500')
             : songData?.images?.coverart}
-          className="sm:w-48 w-28 sm:h-48 w-28 rounded-full object-cover border-2 shadow-xl shadow-black"
+          className="sm:w-48 w-28 sm:h-48 rounded-full object-cover border-2 shadow-xl shadow-black"
         />
 
         <div className="ml-5" >
--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -2,25 +2,27 @@ import { useDispatch, useSelector } from "react-redux";
 
 import { Error, Loader, SongCard } from "../components";
 import { genres } from '../assets/constants';
-import { useGetTopChartsQuery } from '../redux/services/shazamCore';
+import { useGetSongsByGenreQuery } from '../redux/services/shazamCore';
+import { selectGenreListId } from "../redux/features/playerSlice";
 
 const Discover = () => {
     const dispatch = useDispatch();
-    const { activeSong, isPlaying } = useSelector((state) => state.player);
+    const { activeSong, isPlaying, genreListId } = useSelector((state) => state.player);
 
-    const { data, isFetching, error } = useGetTopChartsQuery();
-    const genreTitle = 'Pop';
+    const { data, isFetching, error } = useGetSongsByGenreQuery(genreListId || 'POP');
 
     if (isFetching) return <Loader title="Loading songs..." />
     if (error) return <Error />
+
+    const genreTitle = genres.find(({ value }) => value === genreListId)?.title;
 
     return (
         <div className="flex flex-col" >
             <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10" >
                 <h2 className="font-bold text-3xl text-white text-left" >Discover {genreTitle}</h2>
                 <select
-                    onChange={() => { }}
-                    value=""
+                    onChange={(e) => dispatch(selectGenreListId(e.target.value))}
+                    value={genreListId || 'POP'}
                     className="bg-black text-gray-300 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5"
                 >
                     {
